f8e89968eaa21456fb71e517e2575e83172d01ca
bigdaddy92474@gmail.com <bigdaddy92474@gmail.com>
2013-10-01 11:15:01 -0500

    more apn tweaking
    Change-Id: Ifa3a6c84123406d034e3417d91eccd1c1d1317fd

--- configs/cdma.mk
+++ configs/cdma.mk
@@ -1,7 +1,11 @@
+# CDMA APN list
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
+
+
+
 # specify phone type - needed by 2G toggle
 PRODUCT_PROPERTY_OVERRIDES += \
     gsm.current.phone-type=2
 
-# CDMA APN list
-PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
+
--- configs/gsm.mk
+++ configs/gsm.mk
@@ -1,3 +1,12 @@
+# GSM APN list
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# GSM SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+
 # specify phone type - needed by 2G toggle
 PRODUCT_PROPERTY_OVERRIDES += \
     gsm.current.phone-type=1
