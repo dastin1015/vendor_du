dadad24bf02e8e5645f335689bb1abf55b8da3fd
Alex <mazdarider23@gmail.com>
2013-10-12 22:34:51 -0500

    Added overlay for jewel
    Change-Id: I07945217df21853c23953d189c0fac05866e730c

--- products/jewel.mk
+++ products/jewel.mk
@@ -15,6 +15,9 @@ PRODUCT_MANUFACTURER := HTC
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jewel BUILD_FINGERPRINT="htc/jewel/jewel:4.1.1/JRO03C/147243.3:user/release-keys" PRIVATE_BUILD_DESC="3.16.651.3 CL147243 release-keys"
 
+## overlays
+DEVICE_PACKAGE_OVERLAYS += vendor/du/overlay/jewel
+
 # Release name
 PRODUCT_RELEASE_NAME := jewel
 
